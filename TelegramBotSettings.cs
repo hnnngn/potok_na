2eb78763df421cb9546e1f4cfa2e49c758ed18ea
hnnngn <93208243+hnnngn@users.noreply.github.com>
2022-05-01 19:50:44 +0300

    deleting private settings

--- TelegramBotSettings.cs
+++ TelegramBotSettings.cs
@@ -5,8 +5,8 @@ namespace PotokNewsAgency
     static class TelegramBotSettings
     {
         public const string BotName = "Новостное агентство Поток";
-        public static ChatId MessagesSendingChatId { get; } = new ChatId(-1001697443849);
+        public static ChatId MessagesSendingChatId { get; } = new ChatId(//chatid);
         public const string MessagesInitialSymbol = "⚡️";
-        public const string BotKey = "5346570676:AAEoCAGccKZG0ANg00Wb4EoBbI_sIXJsxB8";
+        public const string BotKey = //botkey;
     }
 }
